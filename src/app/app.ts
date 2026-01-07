d92546b55c48c13434be0d9b25749f6019e83ac2
prasannatrainer80 <prasanna.trainer.java@gmail.com>
2026-01-07 12:45:30 +0530

    code

--- src/app/app.ts
+++ src/app/app.ts
@@ -3,10 +3,11 @@ import { RouterOutlet } from '@angular/router';
 import { First } from './first/first';
 import { Second } from './second/second';
 import { Third } from './third/third';
+import { ButtEx } from './butt-ex/butt-ex';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet,First,Second,Third],
+  imports: [RouterOutlet,First,Second,Third,ButtEx],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
